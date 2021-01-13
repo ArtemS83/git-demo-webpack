749b79dd5ecb9fe50d511b588c754b4e5c707b37
Atrem Sergieiev <a_serg83@ukr.net>
2021-01-13 22:50:42 +0100

    crud-added logout add delete

--- src/js/CRUD-axios-async.js
+++ src/js/CRUD-axios-async.js
@@ -10,8 +10,10 @@ const refs = {
   userName: document.querySelector('.user-info .name'),
   userEmail: document.querySelector('.user-info .email'),
   contentForm: document.querySelector('.content'),
-  form: document.querySelector('.form'),
+  formADD: document.querySelector('.form'),
   listContacs: document.querySelector('.js-list'),
+  textNoContacts: document.querySelector('.span-no-contacts'),
+  formUpdate: document.querySelector('.form-update'),
 };
 
 const BASE_URL = 'https://goit-phonebook-api.herokuapp.com';
@@ -58,6 +60,7 @@ function onGetDataUser(dataValue) {
       refs.signupForm.classList.add('is-hidden');
       refs.contentForm.classList.remove('is-hidden');
       refs.logoutBtn.classList.remove('is-hidden');
+      refs.textNoContacts.classList.remove('is-hidden');
     })
     .catch(error => console.log(error));
 }
@@ -142,21 +145,58 @@ function fetchLogin(data) {
   });
 }
 
+//=================LOGOUT===============//
+refs.logoutBtn.addEventListener('click', onLogout);
+function onLogout() {
+  console.log('onLogout-Token', Token);
+  fetchLogout(Token)
+    .then(() => {
+      Token = '';
+      refs.userName.textContent = '';
+      refs.userEmail.textContent = '';
+      refs.logoutBtn.classList.add('is-hidden');
+      refs.textError.classList.add('is-hidden');
+      refs.loginForm.classList.add('is-hidden');
+      refs.contentForm.classList.add('is-hidden');
+      refs.signupBtn.classList.remove('is-hidden');
+      refs.loginBtn.classList.remove('is-hidden');
+      refs.listContacs.innerHTML = '';
+      refs.textNoContacts.classList.add('is-hidden');
+    })
+    .catch(error => console.log(error));
+}
+//
+function fetchLogout(tokenValue) {
+  const options = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${tokenValue}`,
+    },
+  };
+
+  return fetch(`${BASE_URL}/users/logout`, options).then(res => {
+    if (res.status === 401) {
+      refs.textError.classList.remove('is-hidden');
+      return;
+    } else {
+      return res.json();
+    }
+  });
+}
 //=================getContacs===============//
 function getContacts(tokenValue) {
   console.log('getContacts-Token');
-  git;
   fetchСontacts(tokenValue)
     .then(data => {
       // console.log(data);
-      render(data);
+      renderContacts(data);
     })
     .catch(error => console.log(error));
 }
 
-function fetchСontacts(token1) {
+function fetchСontacts(tokenValue) {
   const headers = {
-    Authorization: `Bearer ${token1}`,
+    Authorization: `Bearer ${tokenValue}`,
   };
 
   return fetch(`${BASE_URL}/contacts`, {
@@ -165,22 +205,174 @@ function fetchСontacts(token1) {
 }
 
 //=============render contacts=======//
-function render(contacts) {
+function renderContacts(contacts) {
   console.log(contacts.length);
+  refs.listContacs.innerHTML = '';
   if (contacts.length <= 0) {
-    refs.listContacs.innerHTML = 'NO CONTACTS';
+    refs.textNoContacts.classList.remove('is-hidden');
+    // refs.listContacs.innerHTML = 'NO CONTACTS';
+    // refs.listContacs.innerHTML = '';
     return;
   }
-  const items = contacts
+  console.log('renderContacts-contacts', contacts);
+  // refs.listContacs.innerHTML = '';
+  render(contacts);
+}
+//===================template contact===========//
+function render(params) {
+  const items = params
     .map(
       ({ id, name, number }) =>
         `<li>
-          ${name}: ${number}
+        <span class="js-name-${id}">${name}: </span> <span class="js-number-${id}">${number}</span>
+        
           <button class="delete" data-id=${id} data-type="delete">delete</button>
           <button class="update" data-id=${id} data-type="update">update</button>
         </li>`,
     )
     .join('');
-
   refs.listContacs.insertAdjacentHTML('beforeend', items);
+}
+//=============ADD contact=======//
+refs.formADD.addEventListener('submit', onAddContact);
+
+function onAddContact(e) {
+  e.preventDefault();
+
+  const name = e.currentTarget.elements.name.value;
+  const number = e.currentTarget.elements.number.value;
+  const newContact = {
+    name,
+    number,
+  };
+  addNewContact(newContact)
+    .then(data => {
+      const item = [];
+      item.push(data);
+      console.log('addNewContact-data', item);
+      render(item);
+      refs.textError.classList.add('is-hidden');
+    })
+    .then(() => {
+      newContact.name = '';
+      newContact.value = '';
+      // console.log(e);
+      refs.formADD.reset();
+      refs.textNoContacts.classList.add('is-hidden');
+    })
+    .catch(error => console.log(error));
+}
+function addNewContact(data) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${Token}`,
+    },
+    body: JSON.stringify(data),
+  };
+
+  return fetch(`${BASE_URL}/contacts`, options).then(res => {
+    if (res.status === 400) {
+      refs.textError.classList.remove('is-hidden');
+      return;
+    } else {
+      return res.json();
+    }
+  });
+}
+//=============DELETE contact=======//
+refs.listContacs.addEventListener('click', handleContactClick);
+
+function handleContactClick(e) {
+  const { id, type } = e.target.dataset;
+
+  if (e.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  if (id && type === 'delete') {
+    console.log(Token, id, type);
+    deleteContact(Token, id)
+      .then(() => {
+        refs.textError.classList.add('is-hidden');
+        getContacts(Token);
+      })
+      .catch(error => console.log(error));
+  }
+  //=============UPdate contact=======//
+  if (id && type === 'update') {
+    const nameContactRef = document.querySelector(`.js-name-${id}`);
+    const numberContactRef = document.querySelector(`.js-number-${id}`);
+    const updateContact = {
+      updateContact: nameContactRef.textContent,
+      number: numberContactRef.textContent,
+    };
+
+    refs.contentForm.classList.add('is-hidden');
+    refs.formUpdate.classList.remove('is-hidden');
+    refs.formUpdate.elements.name.value = updateContact.updateContact;
+    refs.formUpdate.elements.number.value = updateContact.number;
+
+    refs.formUpdate.addEventListener('submit', onUpdateContact);
+
+    function onUpdateContact(e) {
+      e.preventDefault();
+
+      const name = e.target.elements.name.value;
+      const number = e.target.elements.number.value;
+      const updateData = {
+        name,
+        number,
+      };
+      console.log('updateData', updateData, Token, id);
+      updateContact(updateData, Token, id)
+        .then(() => {
+          refs.textError.classList.add('is-hidden');
+          refs.formUpdate.classList.add('is-hidden');
+          refs.formUpdate.reset();
+          getContacts(Token);
+        })
+        .catch(error => console.log(error));
+    }
+  }
+}
+
+function updateContact(data, tokenValue, idValue) {
+  const options = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${tokenValue}`,
+    },
+    body: JSON.stringify(data),
+  };
+
+  return fetch(`${BASE_URL}/contacts/${idValue}`, options).then(res => {
+    if (res.status === 401 || res.status === 400) {
+      refs.textError.classList.remove('is-hidden');
+      return;
+    } else {
+      return res.json();
+    }
+  });
+}
+
+function deleteContact(tokenValue, idValue) {
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${tokenValue}`,
+    },
+  };
+
+  return fetch(`${BASE_URL}/contacts/${idValue}`, options).then(res => {
+    if (res.status === 401 || res.status === 404) {
+      refs.textError.classList.remove('is-hidden');
+      return;
+    } else {
+      return res.json();
+    }
+  });
 }
